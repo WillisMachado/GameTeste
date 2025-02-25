5a89c80e4cc01fc7280172e8a32cdfefe1fffa3e
WillisMachado <willispereiram@gmail.com>
2025-02-25 19:32:24 -0300

    Creating asset folder Inserting all assets Creating the menu Creating the cost file

---
+++ code/Const.py
@@ -0,0 +1,11 @@
+# C
+COLOR_ORANGE = (255, 130, 0)
+COLOR_WHITE = (255, 255, 255)
+
+# M
+MENU_OPTION = ('NEW GAME',
+               'SCORE',
+               'EXIT')
+# W
+WIN_WIDTH = 800
+WIN_HEIGHT = 600
