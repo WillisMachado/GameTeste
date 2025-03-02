2864fa3ae459cfebf26bf9ca0a7768a553729f3b
WillisMachado <willispereiram@gmail.com>
2025-03-02 01:06:47 -0300

    Time, image ans sound alteration

--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -15,13 +15,13 @@ class EntityFactory:
         match entity_name:
             case 'Level1Bg':
                 list_bg = []
-                for i in range(7):  # level1bg images number
+                for i in range(5):  # level1bg images number
                     list_bg.append(Background(f'Level1Bg{i}', (0, 0)))
                     list_bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH, 0)))
                 return list_bg
             case 'Level2Bg':
                 list_bg = []
-                for i in range(5):  # level2bg images number
+                for i in range(4):  # level2bg images number
                     list_bg.append(Background(f'Level2Bg{i}', (0, 0)))
                     list_bg.append(Background(f'Level2Bg{i}', (WIN_WIDTH, 0)))
                 return list_bg
